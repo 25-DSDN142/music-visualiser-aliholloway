043035c954b0a24a9c88140cedf092612107952a
ryanholloway123 <ryanholloway123@gmail.com>
2025-09-14 15:22:57 +1200

    Sun created
    Sun working properly with ellipse for center and triangles for sun rays.

--- music_starter.js
+++ music_starter.js
@@ -1,39 +1,58 @@
 
+
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
    
   background("#0C1713");
 
+  // sun ----- start -------------------------------------------------------
+  noStroke();
+  fill("#ffe600ff");
+  let sun_radius = height * 0.33;
+  let sun_tri_height = 100;     // ANIMATE THIS WITH MUSIC!!!
+  let num_sun_tris = 50;   // controls the number of sun rays and determines how wide each triangle is
+  sun_center_x = width * 0.5;
+  sun_center_y = height * 0.5;
+
+
+  ellipse(width * 0.5, height * 0.5, sun_radius * 2.0, sun_radius * 2.0);
+
+
+  for (let i = 0; i < num_sun_tris; i++) {
+    sun_tri_angle = 360.0 / num_sun_tris * i;
+   
+    sun_tri_angle_incr = 360.0 / num_sun_tris * 0.5;
+    triangle(sun_center_x + cos(sun_tri_angle) * sun_radius, sun_center_y + sin(sun_tri_angle) * sun_radius,
+             sun_center_x + cos(sun_tri_angle + sun_tri_angle_incr) * (sun_radius + sun_tri_height), sun_center_y + sin(sun_tri_angle + sun_tri_angle_incr) * (sun_radius + sun_tri_height),
+             sun_center_x + cos(sun_tri_angle + sun_tri_angle_incr * 2.0)  * sun_radius, sun_center_y + sin(sun_tri_angle + sun_tri_angle_incr * 2.0) * sun_radius);
+  }
+  // sun ----- end -------------------------------------------------------------
+
+
+
 
   // star -----------------------------------
   stroke(255);
   strokeWeight(2);
-  let star_line_radius = 20; 
+  let star_line_radius = 20;
   let num_star_lines = 20;
   center_x = width * 0.5;
   center_y = height * 0.5;
+
 
   for (let i = 0; i < num_star_lines; i++) {
     angle = 360.0 / num_star_lines * i;
     line(center_x, center_y, center_x + cos(angle) * star_line_radius, center_y + sin(angle) * star_line_radius);
   }
 
-  //-----------------------------------------------
-  
+
+
+}
 
 
 
 
 
-
-
-
-
-
-
-
-
-  
 /*
 
   let bar_spacing = height / 10;
@@ -75,14 +94,4 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
   */ 
 
-}
 
-
-
-
-
-
-
-
-
-  
